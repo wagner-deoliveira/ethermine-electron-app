c75cbad8b7119fdad681d741faf72ac1c2bf93a6
Wagner Rosa <wagner.deoliveira@perkinelmer.com>
2021-03-18 22:19:08 +0100

    Get event from login button

--- src/miner_stats.js
+++ src/miner_stats.js
@@ -1,8 +1,4 @@
-require('dotenv').config();
-
 const api = "https://api.ethermine.org/";
-const minerStats = `miner/${process.env.MY_WALLET}/workers`;
-
 let myHeaders = new Headers();
 let myInit = {
     method: 'GET',
@@ -13,17 +9,25 @@ let myInit = {
 
 
 async function getMinerStats() {
-    const response = await fetch(api + minerStats, myInit);
-    if (!response.ok) {
-        const message = `An error has occured: ${response.status}`;
-        throw new Error(message);
+    let login, user = {};
+    login = document.getElementById("login");
+    login.onsubmit = async function (event) {
+        event.preventDefault();
+        user.wallet = document.getElementById("wallet").value;
+        const minerStats = `miner/${user.wallet}/workers`;
+        const response = await fetch(api + minerStats, myInit);
+        if (!response.ok) {
+            const message = `An error has occurred: ${response.status}`;
+            throw new Error(message);
+        }
+        const worker = response.json().then(data => {
+            let listItem = document.createElement('li');
+            listItem.appendChild(
+                document.createElement('strong')
+            ).textContent = worker.Name;
+        })
+        console.log(worker);
     }
-    const worker = response.json().then(data => {
-        let listItem = document.createElement('li');
-        listItem.appendChild(
-            document.createElement('strong')
-        ).textContent = worker.Name;
-    })
-    console.log(worker);
-    return worker;
 }
+
+getMinerStats().then(r => {});

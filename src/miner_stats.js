5dfc5499650c19db5051d3efd053364389113176
Wagner Rosa <wagner.deoliveira@perkinelmer.com>
2021-03-22 15:55:29 +0100

    Add countdown timer

--- src/miner_stats.js
+++ src/miner_stats.js
@@ -37,7 +37,6 @@ async function getMinerStats() {
             document.getElementById("averageHashrate").textContent = averageMegaHash;
             document.getElementById("reportedHashrate").textContent = megaHashReported;
             document.getElementById("time").textContent = convertedTime;
-
         });
 
         const responseRevenue = await fetch(api + minerRevenue, myInit);
@@ -61,8 +60,32 @@ function reload(div){
     container.innerHTML = content;
 }
 
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+function rollingTimer() {
+    const tenMinutes = 60 * 10,
+    display = document.querySelector('#countdown');
+    startTimer(tenMinutes, display);
+}
+
 async function handleClick () {
     reload("server");
+    rollingTimer();
     await getMinerStats();
 }
 
